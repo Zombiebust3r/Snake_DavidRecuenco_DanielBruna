dc2a4ba20b8eff7c31dcc158ef8a0eecb0980465
Daniel Bruna <zombiebust3r@gmail.com>
2016-12-15 20:40:07 +0100

    With XML
    XML .hh included

---
+++ src/Snake_GameProject/XMLreader.hh
@@ -0,0 +1,26 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <XML\rapidxml.hpp>
+
+
+using namespace std;
+
+int main() {
+	ifstream myfile("sampleconfig.xml");
+	rapidxml::xml_document<> doc;
+
+	/* "Read file into vector<char>"  See linked thread above*/
+	vector<char> buffer((istreambuf_iterator<char>(myfile)), istreambuf_iterator<char>());
+
+	buffer.push_back('\0');
+
+	cout << &buffer[0] << endl; /*test the buffer */
+
+	doc.parse<0>(&buffer[0]);
+
+	cout << "Name of my first node is: " << doc.first_node()->name() << "\n";  /*test the xml_document */
+
+
+}
